a367666ff167059c903d28f47f254484f0c07001
Craig Christopher Messi <craig.messi@gmail.com>
2019-09-17 03:59:39 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,20 @@
-# code here!
+class School
+  def initialize(school_name)
+  @school_name = school_name
+  end
+  
+  def roster
+    @roster = roster
+    roster[grade] << school.add_student
+  end
+  
+  class Add_student 
+    def initialize(name, grade)
+      @name = name
+      @grade = grade
+    end
+  end
+  
+  roster  
+    
+end
