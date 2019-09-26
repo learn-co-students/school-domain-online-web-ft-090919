56f554cd99aa6c940c2327758b3ac2125a13f991
CLIFF CRELIN <cliff.crelin@gmail.com>
2019-09-26 23:40:25 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,32 @@
-# code here!
+class School
+  attr_accessor(:name, :roster)
+  
+  def initialize(name, roster={})
+    
+    @name = name
+    @roster = roster
+  
+  end
+  
+  def add_student(name, grade)
+    
+    if @roster[grade] != nil 
+      @roster[grade] << name
+    else
+      @roster[grade] = [name] 
+    
+    end
+  end
+  
+   def grade(grade)
+     @roster[grade]
+   end
+   
+   def sort
+     
+     @roster.each do |x,i|
+       i.sort!
+     end
+   end
+   
+end
